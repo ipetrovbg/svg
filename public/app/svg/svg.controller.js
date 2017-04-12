32b397cae512201e5758e981e1bf2c3f8e3272aa
ipetrovbg <ipetrovbg@gmail.com>
2017-04-12 23:59:07 +0300

    expanding the view port of canvas

--- public/app/svg/svg.controller.js
+++ public/app/svg/svg.controller.js
@@ -6,10 +6,11 @@
     SvgController.$inject = [];
     function SvgController(){
         var vm = this;
-        var draw = SVG('drawing').size(300, 130)
+        var draw = SVG('drawing').size($( '.container' ).width(), $( window ).height());
 
         var path = draw.path('M0 0 L20 30 H50 A20 20 0 1 0 100 50 v25 C50 125 0 85 0 85 z')
-        path.fill('none').move(20, 20)
-        path.stroke({ color: '#f06', width: 4, linecap: 'round', linejoin: 'round' });
+        path.fill('#dadada').move(20, 20)
+        path.stroke({ color: '#f06', width: 1, linecap: 'round', linejoin: 'round' });
+        path.draggable();
     }
 })(SVG, jQuery);
